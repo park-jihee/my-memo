9d37bc620e2e543d7c29f399ddcf637a481c7cce
park-jihee <brs0705@naver.com>
2019-02-27 13:30:42 +0900

    footer.php

---
+++ footer.php
@@ -0,0 +1,9 @@
+        <div id="post-footer">
+            <p>Copyright© 2019.박지희.All rights reserved.</p>
+        </div>
+    </div>
+</body>
+<script src="./js/script.js"></script>
+<script src="./js/search.js"></script>
+<script src="./js/delete.js"></script>
+</html>
